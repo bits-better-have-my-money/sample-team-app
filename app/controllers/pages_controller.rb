011a563891e1b5893ce2a846f6acfd6fc7361d07
Nicole Sanmarco <nicole.sanmarco@yahoo.com>
2021-10-25 20:33:50 -0400

    adds my name to the json

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
   def index
-    render json: {message: "Hello world!"}
+    render json: { message: "Nikki Sanmarco" }
   end
 
   def show
-    render json: {message: "This is a pages show action!"}
+    render json: { message: "This is a pages show action!" }
   end
 end
